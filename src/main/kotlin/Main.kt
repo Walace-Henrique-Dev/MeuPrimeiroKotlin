75557cc5bc52c2ad93ce2d9f4af27cd59e11dd6c
unknown <wuall@hotmail.comgit config --global user.name WalaceHenrique>
2021-10-22 12:19:52 -0300

    MeuPrimeiroKotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("???")
+    println("Opa, eae")
+    println("Rogerio: De boas?")
+    println("Rogerio: Qual o seu nome?")
+    println("???: Meu nome é Henrique")
+    println("Rogerio: Opa, prazer em conhecê-lo Henrique")
+    println("Henrique: O prazer é meu!")
+
+
+
+
+}
